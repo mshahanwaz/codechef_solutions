5fdd41cb250cba82814037618e038d8118d80af8
mshahanwaz <mohdshahanwaz55@gmail.com>
2022-03-02 09:38:29 +0530

    Modified solution

--- magnet_sort.cpp
+++ magnet_sort.cpp
@@ -19,7 +19,40 @@ int main() {
 		}
 		string s;
 		cin >> s;
-		
+		if (is_sorted(a.begin(), a.end())) {
+			cout << 0 << endl;
+			continue;
+		}
+		int cntN = 0;
+		for (char c: s)
+			if (c == 'N')
+				++cntN;
+		if (!cntN || cntN == n) {
+			cout << -1 << endl;
+			continue;
+		}
+		vector<int> b = a;
+		sort(a.begin(), a.end());
+		int start = INT_MAX;
+		int end = INT_MIN;
+		for (int i = 0; i < n; ++i) {
+			if (a[i] != b[i]) {
+				start = min(i, start);
+				end = max(end, i);
+			}
+		}
+		bool f1, f2;
+		f1 = f2 = false;
+		for (int i = 0; !f1 && i < start; ++i) {
+			if (s[i] != s[end]) f1 = true;
+		}
+		for (int i = end; !f2 && i < n; ++i) {
+			if (s[i] != s[start]) f2 = true;
+		}
+		if (s[start] != s[end] || f1 || f2)
+			cout << 1;
+		else cout << 2;
+		cout << endl;
 	}
 	return 0;
 }
