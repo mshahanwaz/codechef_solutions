2c408080ec706a8fe51801b459fb262377144d37
mshahanwaz <mohdshahanwaz55@gmail.com>
2022-03-12 13:02:47 +0530

    Added contest solution

---
+++ subarray_xor.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define endl '\n'
+#define fastIO cin.tie(0)->sync_with_stdio(0)
+#define MOD 998244353
+
+int main() {
+	fastIO;
+	int TC = 1;
+	cin >> TC;
+	while (TC--) {
+		int n;
+		cin >> n;
+		string s;
+		cin >> s;
+		for (int i = 0; i < n / 2; ++i) {
+			int cnt = (k + 1) * (n - k);
+			if (cnt % 2) {
+
+			}
+		}
+	}
+	return 0;
+}
